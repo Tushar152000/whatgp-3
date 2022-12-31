b3d956370b51edea642994e70cdb67ed550dff87
Tushar152000 <chauhantushar538@gmail.com>
2022-12-31 15:35:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React-project-01
---
+++ application/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Features, Footer, Blog, Header,Possibility, WhatGPT3} from './containers';
+import {Brand,CTA,Navbar} from './components';
+
+
+const App = () => {
+  return (
+    <div className='App'>
+    <div className='gradient_bg'>
+      <Navbar />
+      <Header />
+
+    </div>
+     
+    <Brand/> 
+    <WhatGPT3/>
+    <Features/>
+    <Possibility/>
+    <CTA/>
+    <Blog/>
+    <Footer/>
+  </div>
+  );
+
+};
+
+export default App;
+
+
